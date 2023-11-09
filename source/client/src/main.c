4ccf552d4eb8a1eb0dce1f257052775dfe6e69ac
SAMi-Rn <sami_roudgarian@yahoo.com>
2023-11-08 20:34:42 -0800

    Hi

--- source/client/src/main.c
+++ source/client/src/main.c
@@ -23,6 +23,15 @@ int main(int argc, char **argv)
             .argv = argv,
             .args = &args
     };
+    printf("Size of packets: %zu \n", sizeof(packet));
+    printf("Size of header: %zu \n", sizeof(((packet *)0)->hd));
+    printf("Size of date: %zu \n", sizeof(((packet *)0)->data));
+    printf("Size of header: %zu \n", sizeof(header));
+    printf("Size of sequence_number: %zu \n", sizeof(((header*)0)->sequence_number));
+    printf("Size of acknowledgment_number: %zu \n", sizeof(((header*)0)->acknowledgment_number));
+    printf("Size of flags: %zu \n", sizeof(((header*)0)->flags));
+    printf("Size of window_size: %zu \n", sizeof(((header*)0)->window_size));
+    printf("Size of struct timeval: %zu \n", sizeof(((header*)0)->tv));
 
     return 0;
 }
